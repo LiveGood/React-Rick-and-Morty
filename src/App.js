d685a033111b693ccc4f439b442df0d7812089d1
LiveGood <FleshThps@gmail.com>
2020-10-08 13:39:51 +0300

    Added ThemeProvider

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import BrowserHistory from './BrowserHistory'
+import { Router, Route } from 'react-router-dom'
+import styled, { ThemeProvider } from 'styled-components'
 import './index.css';
+
+const AppElement = styled.div`
+  position: relative;
+
+  .animated-router {
+    & > * {
+      width: 100%;
+      position: absolute;
+    }
+  }
+`;
+
 
 function App() {
   return (
-    <div className="App">
-      <div>Hello</div>
-    </div>
+    <ThemeProvider theme={{}}>
+      <AppElement>
+        <div>Hello</div>
+      </AppElement>
+    </ThemeProvider>
   );
 }
 
 export default App;
+
