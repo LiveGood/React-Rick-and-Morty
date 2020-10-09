665561ff6f07741c2eccb8d6bb3a9df40a5a15a5
LiveGood <FleshThps@gmail.com>
2020-10-09 20:10:41 +0300

    Reverted history module to 4.10.1 because it was making NavLinks act badly

--- src/App.js
+++ src/App.js
@@ -7,11 +7,12 @@ import './index.css';
 import ResponsiveRender from './components/ResponsiveRender';
 
 import { Sidebar } from './components'
+// import { CharactersList, EpisodesList, CharacterSingle } from './containers'
+import { CharacterIcon, EpisodeIcon } from './assets/svg'
+import GlobalStyle from './GlobalStyle'
 
 const AppElement = styled.div`
   position: relative;
-
-
   .animated-router {
     & > * {
       width: 100%;
@@ -20,21 +21,73 @@ const AppElement = styled.div`
   }
 `;
 
+const NAV_ITEMS = [
+  {
+    name: 'episodes',
+    icon: EpisodeIcon,
+    to: '/',
+    exact: true,
+  },
+  {
+    name: 'characters',
+    icon: CharacterIcon,
+    to: '/characters',
+    exact: false,
+  },
+]
+
+const ROUTES = [
+  {
+    path: '/',
+    // component: EpisodesList,
+    component: () => { return <div>Episodes</div> },
+  },
+  {
+    path: '/characters',
+    // component: CharactersList,
+    component: () => { return <div>Characters</div> },
+
+  },
+  {
+    path: '/characters/:id',
+    // component: CharacterSingle,
+    component: () => { return <div>Single Character</div> },
+  }
+]
+
 function App() {
   // TODO: add local storage latere
   const themeMode = 'light'
 
   return (
     <ThemeProvider theme={theme[themeMode]}>
-      <Router history={BrowserHistory}>
-        <AppElement>
-          <ResponsiveRender>
-            <Sidebar >
+      <AppElement>
+        <Router history={BrowserHistory}>
+            <ResponsiveRender>
+              <Sidebar {... {navItems: NAV_ITEMS}} />
+            </ResponsiveRender>
 
-            </Sidebar>
-          </ResponsiveRender>
-        </AppElement>
-      </Router>
+
+          <Route path="/">
+            {ROUTES[0].component}
+          </Route>
+
+          <Route path="/characters">
+            {ROUTES[1].component}
+          </Route>
+          {/* {ROUTES.map(({ path, component, ...restProps })=> {
+            return (
+              <Route
+                key={`route-${path}`}
+                exact
+                {...{ path, component }}
+                {...restProps}
+              />
+            )
+          })} */}
+        </Router>
+      </AppElement>
+      {/* <GlobalStyle /> */}
     </ThemeProvider>
   );
 }
