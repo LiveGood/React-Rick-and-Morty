c58d438177aa7de5f0b9065d5b4a6e306dbc8f17
LiveGood <FleshThps@gmail.com>
2020-10-26 19:52:03 +0200

    Added TabNavWrapper and TabNav ul

--- src/components/TabContent.js
+++ src/components/TabContent.js
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useEffect, useContext } from 'react';
+import styled, { css } from 'styled-components';
+
+const TabContentWrapper = styled.div`
+  box-shadow: ${({ theme }) => theme.materialShadow};
+  position: relative;
+  border-radius: 5px;
+  overflow: hidden;
+`; 
+
+const TabNav = styled.ul`
+  position: relative;
+  list-style: none;
+  overflow: hidden;
+  display: flex;
+  flex-wrap: wrap;
+`;
 
 function TabContent() {
   return (
-    <div>
-      
-    </div>
+    <TabContentWrapper>
+      <TabNav>
+        
+      </TabNav>
+    </TabContentWrapper>
   )
 }
 
